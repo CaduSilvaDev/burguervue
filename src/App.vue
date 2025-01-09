f23e56eec484882cb34ab139cd0b804626420519
CaduSilva28 <edu420092@gmail.com>
2025-01-09 15:12:02 -0300

    Aula 23
    Subindo arquivos feitos até a aula 23

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,46 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view/>
+  <div id="app">
+    <NavbarComponent :logo="logo_src" :alt="logo_alt" />
+    <router-view/>
+    <FooterComponent />
+  </div>
 </template>
 
+<script> 
+import NavbarComponent from "./components/NavbarComponent.vue";
+import FooterComponent from './components/FooterComponent.vue';
+
+export default {
+  components: {
+    NavbarComponent,
+    FooterComponent
+  },
+  data(){
+    return {
+      logo_src: "/img/logo.png",
+      logo_alt: "Make Your Burguer"
+    }
+  }
+}
+</script>
+
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
+ * {
+  font-family: Helvetica;
+  padding: 0;
+  margin: 0;
+  box-sizing: border-box;
+ }
+ 
+ .main-container{
+  margin: 50px;
+  min-height: 250px;
+ }
+
+ h1{
   text-align: center;
-  color: #2c3e50;
-}
-
-nav {
-  padding: 30px;
-}
-
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-nav a.router-link-exact-active {
-  color: #42b983;
-}
+  font-size: 42px;
+  margin-bottom: 30px;
+  color: #222;
+ }
 </style>
