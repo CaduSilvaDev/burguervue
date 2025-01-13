be4df85f46157656e06797de2557c487b4812a4b
CaduSilva28 <edu420092@gmail.com>
2025-01-12 22:08:30 -0300

    Subindo até aula 26

--- src/components/BannerComponent.vue
+++ src/components/BannerComponent.vue
@@ -25,7 +25,7 @@
         color: #FFF;
         text-align: center;
         font-size: 60px;
-        
+        background-color: #222;
         padding: 20px 40px;
     }
 </style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,17 +2,19 @@
   <BannerComponent />
   <div class="main-container">
     <h1>Monte o seu burger</h1>
-    <p>Formulário</p>
+    <BurgerFormComponent />
   </div>
 </template>
 
 <script>
 import BannerComponent from '../components/BannerComponent.vue';
+import BurgerFormComponent from '../components/BurgerFormComponent.vue';
 
 export default {
   name: 'HomeView',
   components: {
-    BannerComponent
+    BannerComponent,
+    BurgerFormComponent
   }
 }
 </script>
